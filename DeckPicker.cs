d01debd1a6f6cef9c24b7a84ecc51fcd5395e394
ADollar4750 <Alextdollar@gmail.com>
2022-12-03 09:17:29 -0700

    Add file

--- DeckPicker.cs
+++ DeckPicker.cs
@@ -1,3 +1,19 @@
 ﻿using DeckPrint;
+
+string [] arrays= {};
+ String sdira= "path of the directory";
+
+List<string> files = new List<string>();
+String decklist = "path of the directory";
+
+foreach (string d in Directory.GetDirectories(sdira)) {
+
+    foreach (string f in Directory.GetFiles(d, "*.*")) {
+       files.Add(f);
+    }
+}
+
+Console.WriteLine("Please pick a deck or create a new one: ");
+
 Printer loreHold = new Printer();
 loreHold.PrintDeck();
