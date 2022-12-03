adc9fd1eca0a2d6a094155ced62d9fe7dccc7165
ADollar4750 <Alextdollar@gmail.com>
2022-12-03 09:24:51 -0700

    C

--- DeckPicker.cs
+++ DeckPicker.cs
@@ -1,19 +1,19 @@
 ﻿using DeckPrint;
 
-string [] arrays= {};
- String sdira= "path of the directory";
-
-List<string> files = new List<string>();
-String decklist = "path of the directory";
-
-foreach (string d in Directory.GetDirectories(sdira)) {
-
-    foreach (string f in Directory.GetFiles(d, "*.*")) {
-       files.Add(f);
-    }
+var files = new List<string>(); 
+string sdira= "path of the directory"; 
+foreach (string d in Directory.GetDirectories(sdira)) {   
+    files.AddRange(Directory.GetFiles(d, "*.*"));   
 }
 
+// To use
+foreach(string file in files) { }
+string file2 = files[2]; 
+// or convert to array
+string[] arrays = files.ToArray();
+
 Console.WriteLine("Please pick a deck or create a new one: ");
+
 
 Printer loreHold = new Printer();
 loreHold.PrintDeck();
